54d21c31649088760acba0320547e39f3e347e00
SilentInt <silentint@foxmail.com>
2023-11-30 13:03:25 +0800

    Modified font size

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono NFM:style=Regular:pixelsize=20:antialias=true:autohint=true";
+static char *font = "JetBrainsMono NFM:style=Regular:pixelsize=22:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
